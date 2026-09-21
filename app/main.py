4a1276f10ee55eee8245139cffbaf2360c3225f2
devfcoelho <dafonsecafelipecoelho@gmail.com>
2026-09-20 22:33:31 -0300

    Configura conexão com PostgreSQL usando .env

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask.cli import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db = SQLAlchemy(app)
 @app.route("/")
 def inicio():
     return "Olá, Slotly"
