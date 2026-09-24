75ddd9c56f760b9f60cf70f9d0de6efa7cb3793c
devfcoelho <dafonsecafelipecoelho@gmail.com>
2026-09-24 16:05:53 -0300

    Cria modelo Servico e testa conexão com o banco

--- app/main.py
+++ app/main.py
@@ -11,3 +11,18 @@ db = SQLAlchemy(app)
 @app.route("/")
 def inicio():
     return "Olá, Slotly"
+
+class Servico(db.Model):
+    __tablename__ = "servico"
+
+    id = db.Column(db.Integer, primary_key =True)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.String(255))
+    duracao_min = db.Column(db.Integer, nullable=False)
+    preco = db.Column(db.Numeric(8,2), nullable=False)
+    ativo = db.Column(db.Boolean, nullable=False, default=True)
+
+    __table_args__ = (
+        db.CheckConstraint("duracao_min > 0", name="ck_servico_duracao"),
+        db.CheckConstraint("preco >= 0", name="ck_servico_preco"),
+    )
